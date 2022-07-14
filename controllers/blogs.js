578852ef417ed9f2729fad34294fc71c54b6e10f
Zishan <zishansiddique585@gmail.com>
2022-07-14 21:29:14 +0545

    added api to fetch blogs info with specific id also added api to post blogs into the database

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,4 +10,34 @@ blogsRouter.get("/", (request, response, next) => {
     .catch((err) => next(err));
 });
 
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.json(result);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
+});
+
+blogsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  const newBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  newBlog
+    .save()
+    .then((result) => {
+      response.json(result);
+    })
+    .catch((err) => next(err));
+});
+
 module.exports = blogsRouter;
