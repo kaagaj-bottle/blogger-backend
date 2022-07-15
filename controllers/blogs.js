e90b37a84b0a83798fa026b447a5570281fd846f
Zishan <zishansiddique585@gmail.com>
2022-07-15 21:51:05 +0545

    added function to call delete and put put changes the number of likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -30,4 +30,23 @@ blogsRouter.post("/", async (request, response) => {
   response.json(savedBlog);
 });
 
+blogsRouter.delete("/:id", async (request, response) => {
+  Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put("/:id", async (request, response) => {
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, {
+    likes: request.body.likes,
+  });
+  response.json(updatedBlog);
+});
+
 module.exports = blogsRouter;
