8034150eef21c24921c75b59959921c6afc34662
Zishan <zishansiddique585@gmail.com>
2022-07-17 22:22:16 +0545

    dummy test working fine

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,7 +1,15 @@
 const listHelper = require("../utils/list_helper");
 
 describe("total likes", () => {
-  const listWithOneBlog = [
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
     {
       _id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
@@ -10,11 +18,43 @@ describe("total likes", () => {
       likes: 5,
       __v: 0,
     },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0,
+    },
   ];
 
   test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(5);
+    const result = listHelper.totalLikes([blogs[0]]);
+    expect(result).toBe(blogs[0].likes);
   });
 
   test("of empty list is zero", () => {
@@ -22,10 +62,8 @@ describe("total likes", () => {
     expect(result).toBe(0);
   });
 
-  list = [{ likes: 10 }, { likes: 399 }, { likes: 32 }];
-
   test("of a bigger list is calculated right", () => {
-    const result = listHelper.totalLikes(list);
-    expect(result).toBe(10 + 399 + 32);
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(7 + 5 + 12 + 10 + 2);
   });
 });
