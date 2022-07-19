9b7de3d2af7f6c1a330e3d3e5ff3d33a5eb9fc0e
Zishan <zishansiddique585@gmail.com>
2022-07-20 01:27:47 +0545

    all tests working fine

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
+const Blog = require("../models/blog");
+const { blogs, blogsInDatabase } = require("./test_helper");
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  console.log("cleared");
+
+  blogs.forEach(async (blog) => {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+    console.log("saved");
+  });
+  console.log("done");
+});
 
 describe("test on database", () => {
   test("blogs are returned as json", async () => {
@@ -10,7 +23,28 @@ describe("test on database", () => {
       .get("/api/blogs")
       .expect(200)
       .expect("Content-Type", /application\/json/);
-  });
+  }, 1000000);
+
+  test("adding new blog", async () => {
+    const newBlog = new Blog({
+      title: "Dunno",
+      author: "also dunno",
+      url: "i don't know",
+      likes: 122,
+    });
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  }, 1000000);
+
+  test("number of blogs returned is correct", async () => {
+    const res = await api.get("/api/blogs");
+
+    expect(res.body).toHaveLength(blogs.length);
+  }, 10000000);
 
   afterAll(() => {
     mongoose.connection.close();
