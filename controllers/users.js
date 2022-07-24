fcb2cb8aff8747c9620a40132d352662e024502d
Zishan <zishansiddique585@gmail.com>
2022-07-24 18:59:49 +0545

    creating new user works now

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,38 @@
 const User = require("../models/user");
 require("express-async-errors");
+const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({});
   response.json(users);
+});
+
+usersRouter.post("/", async (request, response) => {
+  console.log(request.body);
+  const { username, name, password } = request.body;
+  const doesUserExist = await User.findOne({ username });
+
+  if (doesUserExist) {
+    return response.status(400).json({
+      error: `${doesUserExist.username} already exists}`,
+    });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds, (err, hash) => {
+    console.log(err);
+    console.log(hash);
+  });
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
 });
 
 module.exports = usersRouter;
